d7af7fa5241326b2095d47cbaa4fa5eae7ef9d67
Kristjan Lääne <xonproject@gmail.com>
2024-01-20 13:43:02 +0200

    store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
 function orderCake() {
@@ -9,7 +12,6 @@ function orderCake() {
 
 const initialState = {
   numOfCakes: 10,
-  anotherProperty: 0,
 };
 
 const reducer = (state = initialState, action) => {
@@ -23,3 +25,16 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log('update state', store.getState())
+);
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
